8205302188cb79c3d11ca3927f34f324d3adc496
silu-07 <90642615+silu-07@users.noreply.github.com>
2024-11-11 14:48:28 +0530

    clean up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,11 @@
-import { Component, ViewChild } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterLink, RouterOutlet } from '@angular/router';
-import { ChildComponent } from "./components/child/child.component";
-import { FormsModule } from '@angular/forms';
-import { LifecycleComponent } from "./components/lifecycle/lifecycle.component";
-
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, ChildComponent, FormsModule, LifecycleComponent],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'interview';
-  message: string = '';
-  parentValue: string = '';
 
-  @ViewChild(ChildComponent) childComponent!: ChildComponent;
-
-  // Function to receive data from child component
-  receiveMessage(message: string) {
-    this.message = message;
-  }
-
-  callChildMethod() {
-    this.childComponent.childMethod();
-  }
 }
